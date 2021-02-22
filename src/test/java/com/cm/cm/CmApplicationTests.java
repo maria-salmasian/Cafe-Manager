8eed5a35748a7557caf3207a3a72daef2be3e351
maria-salmasian <maria.salmasian@gmail.com>
2021-02-22 20:14:19 +0400

    first commit

---
+++ src/test/java/com/cm/cm/CmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
